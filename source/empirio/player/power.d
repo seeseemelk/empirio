00d7d17dd353728c6956d94fc63c684145fab4ee
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2021-01-02 13:43:15 +0100

    Add power counter to backend

--- source/empirio/player/power.d
+++ source/empirio/player/power.d
@@ -1,6 +1,8 @@
 module empirio.player.power;
 
 import core.time;
+import std.algorithm;
+import std.math;
 
 /**
 Counts the power a player has at any given time.
@@ -14,7 +16,17 @@ Counts the power a player has at any given time.
     Returns:
         The current power value.
     */
-    long get() const @safe
+	long get() const @safe
+	{
+		return get(time);
+	}
+
+	/**
+	Gets the time since the last click.
+	Returns:
+		The time since the last click in milliseconds.
+	*/
+    long time() const @safe
     {
 		immutable now = MonoTime.currTime;
 		immutable duration = now - start;
@@ -33,6 +45,23 @@ Counts the power a player has at any given time.
 		immutable duration = now - start;
 		immutable milliseconds = duration.total!"msecs";
         start = now;
-		return milliseconds;
+		return get(milliseconds);
     }
+
+	/**
+	Get the power for a given number of milliseconds.
+	*/
+	private static uint get(long milliseconds) @safe
+	{
+		return cast(uint) min(lround(pow(milliseconds / 1000f, 2)), 999);
+	}
+}
+
+@("PowerCounter.get() returns correct values")
+unittest
+{
+	assert(PowerCounter.get(0) == 0);
+	assert(PowerCounter.get(1_000) == 1);
+	assert(PowerCounter.get(10_000) == 100);
+	assert(PowerCounter.get(40_000) == 999);
 }
