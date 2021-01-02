a72682fb699875d28efaaad118767341f9ee0ecf
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2021-01-02 12:01:04 +0100

    Send all tiles and player info on join

--- source/empirio/player/human.d
+++ source/empirio/player/human.d
@@ -8,59 +8,63 @@ import empirio.room;
 import optional;
 import std.algorithm;
 import std.uuid;
+import vibe.core.log;
 
 /**
 Describes a Player who is controlled by a human.
 */
 final class HumanPlayer : Player, RoomObserver
 {
-    private Room _room;
-    private Socket _socket;
-    private string _colour;
-    private string _name;
-    private UUID _uuid;
+	private Room _room;
+	private Socket _socket;
+	private string _colour;
+	private string _name;
+	private UUID _uuid;
 
-    /**
-    Creates a new human player.
-    Params:
-        socket = The socket to communicate over.
-        room = The room in which the player will play.
-        name = The name of the human player.
-        colour = The colour of the player.
-    */
-    this(Socket socket, Room room, string name, string colour)
-    {
-        _colour = colour;
-        _name = name;
-        _room = room;
-        _socket = socket;
-        _uuid = randomUUID();
+	/**
+	Creates a new human player.
+	Params:
+		socket = The socket to communicate over.
+		room = The room in which the player will play.
+		name = The name of the human player.
+		colour = The colour of the player.
+	*/
+	this(Socket socket, Room room, string name, string colour)
+	{
+		_colour = colour;
+		_name = name;
+		_room = room;
+		_socket = socket;
+		_uuid = randomUUID();
 
 		_room.addObserver(this);
-    }
+	}
 
 	/**
 	Sends the entire map to the player.
 	*/
 	void sendMap()
 	{
+		_room.players()
+			.each!(player => onPlayerJoined(player));
+		_room.findNonEmptyTiles().each!(tile => sendTile(tile));
 		_socket.send(ServerMapLoadedPacket());
 	}
 
-    override UUID id() const
-    {
-        return _uuid;
-    }
+	override UUID id() const
+	{
+		return _uuid;
+	}
 
-    override string name() const
-    {
-        return _name;
-    }
+	override string name() const
+	{
+		return _name;
+	}
 
-    override string colour() const
-    {
-        return _colour;
-    }
+	override string colour() const
+	{
+		return _colour;
+	}
 
 	override void onPlayerJoined(Player player)
 	{
@@ -76,15 +80,7 @@ final class HumanPlayer : Player, RoomObserver
 
 	override void onTileChanged(Tile _, Tile newTile)
 	{
-		ServerTileChangePacket packet;
-		newTile.owner.each!((Player owner)
-		{
-			packet.owner = owner.id.toString();
-		});
-		packet.x = newTile.x;
-		packet.y = newTile.y;
-		packet.strength = newTile.strength;
-		_socket.send(packet);
+		sendTile(newTile);
 	}
 
 	/**
@@ -99,24 +95,37 @@ final class HumanPlayer : Player, RoomObserver
 		packet.playerId = _uuid.toString();
 		_socket.send(packet);
 	}
+
+	private void sendTile(Tile tile)
+	{
+		ServerTileChangePacket packet;
+		tile.owner.each!((Player owner)
+		{
+			packet.owner = owner.id.toString();
+		});
+		packet.x = tile.x;
+		packet.y = tile.y;
+		packet.strength = tile.strength;
+		_socket.send(packet);
+	}
 }
 
 @("HumanPlayer.name() returns the name")
 unittest
 {
-    import mocked : Mocker;
-    Mocker mocker;
-    auto socket = mocker.mock!Socket();
-    const player = new HumanPlayer(socket, new Room(1), "hello", "F00");
-    assert(player.name() == "hello");
+	import mocked : Mocker;
+	Mocker mocker;
+	auto socket = mocker.mock!Socket();
+	const player = new HumanPlayer(socket, new Room(1), "hello", "F00");
+	assert(player.name() == "hello");
 }
 
 @("HumanPlayer.colour() returns the colour")
 unittest
 {
-    import mocked : Mocker;
-    Mocker mocker;
-    auto socket = mocker.mock!Socket();
-    const player = new HumanPlayer(socket, new Room(1), "hello", "F00");
-    assert(player.colour() == "F00");
+	import mocked : Mocker;
+	Mocker mocker;
+	auto socket = mocker.mock!Socket();
+	const player = new HumanPlayer(socket, new Room(1), "hello", "F00");
+	assert(player.colour() == "F00");
 }
--- source/empirio/room/room.d
+++ source/empirio/room/room.d
@@ -14,11 +14,11 @@ Contains the settings set at creation of a room.
 */
 struct RoomSettings
 {
-    /// The width of the room.
-    int width = 20;
+	/// The width of the room.
+	int width = 20;
 
-    /// The height of the room.
-    int height = 20;
+	/// The height of the room.
+	int height = 20;
 }
 
 /**
@@ -26,24 +26,24 @@ An empirio room.
 */
 class Room
 {
-    private immutable int _id;
-    private immutable RoomSettings _settings;
-    private Player[] _players;
-    private RoomController _controller;
-    private RoomObserver[] _observers;
-    private Tile[][] _tiles;
+	private immutable int _id;
+	private immutable RoomSettings _settings;
+	private Player[] _players;
+	private RoomController _controller;
+	private RoomObserver[] _observers;
+	private Tile[][] _tiles;
 
-    /**
-    Creates a new room.
-    Params:
-        id = The id of the room.
-        settings = The settings to create the room with.
-    */
-    this(int id, RoomSettings settings = RoomSettings())
-    {
-        _id = id;
-        _settings = settings;
-        _tiles = new Tile[][](settings.height, settings.width);
+	/**
+	Creates a new room.
+	Params:
+		id = The id of the room.
+		settings = The settings to create the room with.
+	*/
+	this(int id, RoomSettings settings = RoomSettings())
+	{
+		_id = id;
+		_settings = settings;
+		_tiles = new Tile[][](settings.height, settings.width);
 
 		foreach (y, ref row; _tiles)
 		{
@@ -55,93 +55,93 @@ class Room
 			}
 		}
 
-        _controller = new ClassicController;
+		_controller = new ClassicController;
 
 		_controller.room = this;
-    }
+	}
 
-    /**
-    Gets the ID of the room.
-    */
-    int id() const pure
-    {
-        return _id;
-    }
+	/**
+	Gets the ID of the room.
+	*/
+	int id() const pure
+	{
+		return _id;
+	}
 
-    /**
-    Gets the settings used when creating the room.
-    */
-    immutable(RoomSettings) settings() const pure
-    {
-        return _settings;
-    }
+	/**
+	Gets the settings used when creating the room.
+	*/
+	immutable(RoomSettings) settings() const pure
+	{
+		return _settings;
+	}
 
-    /**
-    Gets the room controller.
-    */
-    RoomController controller()
-    {
-        return _controller;
-    }
+	/**
+	Gets the room controller.
+	*/
+	RoomController controller()
+	{
+		return _controller;
+	}
 
-    /**
-    Gets a tile.
-    */
-    Tile getTile(int x, int y)
-    {
-        Tile tile = _tiles[x][y];
-        tile.x = x;
-        tile.y = y;
-        return tile;
-    }
+	/**
+	Gets a tile.
+	*/
+	Tile getTile(int x, int y)
+	{
+		Tile tile = _tiles[x][y];
+		tile.x = x;
+		tile.y = y;
+		return tile;
+	}
 
-    /**
-    Sets a tile.
-    */
-    void setTile(Tile tile)
-    {
+	/**
+	Sets a tile.
+	*/
+	void setTile(Tile tile)
+	{
 		Tile oldTile = _tiles[tile.x][tile.y];
 		if (tile != oldTile)
 			observers.each!(observer => observer.onTileChanged(oldTile, tile));
-        _tiles[tile.x][tile.y] = tile;
-    }
+		_tiles[tile.x][tile.y] = tile;
+	}
 
-    /**
-    Gets the players which are in the room.
-    */
-    Player[] players()
-    {
-        return _players;
-    }
+	/**
+	Gets the players which are in the room.
+	*/
+	Player[] players()
+	{
+		return _players;
+	}
 
-    /**
-    Adds a player to the room.
-    */
-    void addPlayer(Player player)
-    {
-        _players ~= player;
-        foreach (observer; _observers)
-            observer.onPlayerJoined(player);
+	/**
+	Adds a player to the room.
+	*/
+	void addPlayer(Player player)
+	{
+		_players ~= player;
+		foreach (observer; _observers)
+			observer.onPlayerJoined(player);
 		_controller.onPlayerJoined(player);
-    }
+	}
 
-    /**
-    Gets the observers which are observing the room.
-    */
-    RoomObserver[] observers()
-    {
-        return _observers;
-    }
+	/**
+	Gets the observers which are observing the room.
+	*/
+	RoomObserver[] observers()
+	{
+		return _observers;
+	}
 
-    /**
-    Adds an observer to the room.
-    Params:
-        observer = The observer to add.
-    */
-    void addObserver(RoomObserver observer)
-    {
-        _observers ~= observer;
-    }
+	/**
+	Adds an observer to the room.
+	Params:
+		observer = The observer to add.
+	*/
+	void addObserver(RoomObserver observer)
+	{
+		_observers ~= observer;
+	}
 
 	/**
 	Finds a tile which satifies the predicate.
@@ -188,55 +188,77 @@ class Room
 	{
 		return findAll(tile => tile.type == TileType.unowned);
 	}
+
+	/**
+	Finds all tiles which are not empty.
+	*/
+	auto findNonEmptyTiles()
+	{
+		return findAll(tile => tile.type != TileType.unowned);
+	}
 }
 
-@("Room.controller() returns the controller of the room")
-unittest
+version(unittest)
 {
-    Room room = new Room(0);
-    assert(room.controller !is null);
-}
+	import mocked;
 
-@("Room.id() returns the id of the room")
-unittest
-{
-    Room room = new Room(1337);
-    assert(room.id == 1337);
-}
+	@("Room.controller() returns the controller of the room")
+	unittest
+	{
+		Room room = new Room(0);
+		assert(room.controller !is null);
+	}
 
-@("Room.players() returns the players in the room")
-unittest
-{
-    import mocked : Mocker;
-    Mocker mocker;
-    Player player = mocker.mock!Player();
-    Room room = new Room(0);
-    room.addPlayer(player);
-    assert(room.players == [player]);
-}
+	@("Room.id() returns the id of the room")
+	unittest
+	{
+		Room room = new Room(1337);
+		assert(room.id == 1337);
+	}
 
-@("Room.observers() returns all observers")
-unittest
-{
-    import mocked : Mocker;
-    Mocker mocker;
-    RoomObserver observer = mocker.mock!RoomObserver();
-    Room room = new Room(0);
-    room.addObserver(observer);
-    assert(room.observers.length == 2);
-    assert(room.observers[1] == observer);
-}
+	@("Room.players() returns the players in the room")
+	unittest
+	{
+		Mocker mocker;
+		Player player = mocker.mock!Player();
+		Room room = new Room(0);
+		room.addPlayer(player);
+		assert(room.players == [player]);
+	}
+
+	@("Room.observers() returns all observers")
+	unittest
+	{
+		Mocker mocker;
+		RoomObserver observer = mocker.mock!RoomObserver();
+		Room room = new Room(0);
+		room.addObserver(observer);
+		assert(room.observers.length == 1);
+		assert(room.observers.any!(o => o == observer));
+	}
 
 
-@("Observers are notified when a player joins")
-unittest
-{
-    import mocked : Mocker;
-    Mocker mocker;
-    auto observer = mocker.mock!RoomObserver();
-    auto player = mocker.mock!Player();
-    Room room = new Room(0);
-    observer.expect.onPlayerJoined(player);
-    room.addObserver(observer);
-    room.addPlayer(player);
+	@("Observers are notified when a player joins")
+	unittest
+	{
+		Mocker mocker;
+		auto observer = mocker.mock!RoomObserver();
+		auto player = mocker.stub!Player();
+		Room room = new Room(0);
+		observer.expect.onPlayerJoined(player);
+		observer.expect.onTileChanged();
+		room.addObserver(observer);
+		room.addPlayer(player);
+	}
+
+	@("Room.findNonEmptyTiles() finds non-empty tiles")
+	unittest
+	{
+		Mocker mocker;
+		Room room = new Room(0);
+		Tile aTile = room.getTile(5, 4);
+		aTile.owner = mocker.stub!Player();
+		room.setTile(aTile);
+		assert(room.findNonEmptyTiles().all!(tile => tile == aTile));
+	}
 }
